cedeb8339543f7c2dc1f1343cd1d514d393c14f0
dagofonseca <dagofonseca6@gmail.com>
2019-05-23 15:04:31 -0500

    eslint fixed

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var lint = require('gulp-eslint');
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css'            
+        ],
+        dist: './dist',
+        mainJs: './src/main.js'
+    }
+}
+
+//Start a local development server
+gulp.task('connect', function (done) {
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    });
+    done();
+});
+
+gulp.task('open', gulp.series('connect', function (done) {
+    gulp.src('dist/index.html')
+        .pipe(open({ uri: config.devBaseUrl + ":" + config.port + "/" }));
+    done();
+}));
+
+gulp.task('html', function (done) {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+    done();
+});
+
+gulp.task('js', function (done) {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+    done();
+});
+
+gulp.task('css', function(done) {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+    done();
+})
+
+gulp.task('lint', function () {
+    return gulp.src(config.paths.js)
+        .pipe(lint())
+        .pipe(lint.format());        
+})
+
+gulp.task('watch', function () {
+    gulp.watch(config.paths.html).on('change', gulp.series('html'));
+    gulp.watch(config.paths.js).on('change', gulp.series('js','lint'));
+})
+
+gulp.task('default', gulp.series('html', 'js', 'css', 'lint', 'open', 'watch'));
---
+++ src/components/homePage.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div className="jumbotron">
+                <h1>Pluralsight Administration</h1>
+            <p>React, React Router and FLux for ultra responsive web apps</p>
+            </div>
+        );
+    }
+}
+
+module.exports = Home;
